c63138786c6b8fb75bbc833ed7c8691ebde7eaa6
opqrstuvcut <vikz2713@gmail.com>
2019-06-07 02:33:11 +0900

    クラス化

--- markov_chain_text_gen/markov_chain.py
+++ markov_chain_text_gen/markov_chain.py
@@ -31,61 +31,6 @@ def read_text(path):
     return texts
 
 
-def make_dist(texts, ngrams=3):
-    dist_dict = dict()
-    for text in texts:
-        text = ["" for _ in range(ngrams)] + text
-        for word_ind, word in enumerate(text):
-            ngram_words = tuple(text[word_ind - ngrams: word_ind])
-
-            if ngram_words not in dist_dict.keys():
-                dist_dict[ngram_words] = defaultdict(int)
-
-            dist_dict[ngram_words][word] += 1
-
-    return dist_dict
-
-
-def generate_text(dist_dict, ngrams=3, max_size=3):
-    while True:
-        ngram_words = tuple(["" for _ in range(ngrams)])
-        generated_text = []
-        phrase_point_num = 0
-        zero_ind_num = 0
-        phrase_word_num = 0
-        while ngram_words in dist_dict.keys():
-            word_dist = dist_dict[ngram_words]
-            keys = list(word_dist.keys())
-            counts = np.array(list(word_dist.values()))
-            prob = counts / np.sum(counts)
-            word_ind = np.where(np.random.multinomial(1, prob) == 1)[0][0]
-            word = keys[word_ind]
-            generated_text.append(word)
-            ngram_words = tuple([ngram_words[1 + i] if i != ngrams - 1 else word for i in range(ngrams)])
-
-            if phrase_word_num >= 20:
-                break
-
-            if word == "。":
-                phrase_point_num += 1
-                phrase_word_num = 0
-                if phrase_point_num >= max_size:
-                    break
-            else:
-                phrase_word_num += 1
-
-            if word_ind != 0:
-                zero_ind_num += 1
-
-        if phrase_word_num >= 20:
-            continue
-
-        if zero_ind_num > 4:
-            break
-
-    return generated_text
-
-
 def unify_text(texts):
     unification_dict = {}
     for text in texts:
@@ -100,12 +45,78 @@ def unify_text(texts):
     return texts
 
 
+class Generator(object):
+    def __init__(self, ngrams=3, max_size=3, max_words=20, text_complex=4):
+        self._dist_dict = None
+        self._ngrams = ngrams
+        self._max_size = max_size
+        self._max_words = max_words
+        self._text_complex = text_complex
+
+    def make_dist(self, texts):
+        dist_dict = dict()
+        for text in texts:
+            text = ["" for _ in range(self._ngrams)] + text
+            for word_ind, word in enumerate(text):
+                ngram_words = tuple(text[word_ind - self._ngrams: word_ind])
+
+                if ngram_words not in dist_dict.keys():
+                    dist_dict[ngram_words] = defaultdict(int)
+
+                dist_dict[ngram_words][word] += 1
+
+        self._dist_dict = dist_dict
+
+    def generate_text(self):
+        while True:
+            ngram_words = tuple(["" for _ in range(self._ngrams)])
+            generated_text = []
+            phrase_point_num = 0
+            zero_ind_num = 0
+            phrase_word_num = 0
+            while ngram_words in self._dist_dict.keys():
+                word_dist = self._dist_dict[ngram_words]
+                keys = list(word_dist.keys())
+                counts = np.array(list(word_dist.values()))
+                prob = counts / np.sum(counts)
+                word_ind = np.where(np.random.multinomial(1, prob) == 1)[0][0]
+                word = keys[word_ind]
+                generated_text.append(word)
+                ngram_words = tuple([ngram_words[1 + i] if i != self._ngrams -
+                                     1 else word for i in range(self._ngrams)])
+
+                if phrase_word_num > self._max_words:
+                    break
+
+                if word == "。":
+                    phrase_point_num += 1
+                    phrase_word_num = 0
+                    if phrase_point_num >= self._max_size:
+                        break
+                else:
+                    phrase_word_num += 1
+
+                if word_ind != 0:
+                    zero_ind_num += 1
+
+            if phrase_word_num > self._max_words:
+                continue
+
+            if zero_ind_num > self._text_complex:
+                break
+
+        return generated_text
+
+
 def main():
     args = parse_argument()
     parsed_text = read_text(args.input)
     parsed_text = unify_text(parsed_text)
-    dist = make_dist(parsed_text, args.ngram)
-    generated_text = generate_text(dist, args.ngram, args.max_size)
+
+    gen = Generator(args.ngram, args.max_size, 20, 4)
+    gen.make_dist(parsed_text)
+    generated_text = gen.generate_text()
+
     print("".join(generated_text))
 
 
